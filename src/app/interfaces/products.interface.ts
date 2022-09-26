c106899fdb00413a63fffe6b43c4cbcea8118da2
Emmanuel <emmanuelescobar2002@gmail.com>
2022-09-26 14:37:15 -0500

    add facture created

--- src/app/interfaces/products.interface.ts
+++ src/app/interfaces/products.interface.ts
@@ -4,4 +4,5 @@ export interface IProducts {
   category: 'hogar' | 'variado';
   description:string;
   price: number;
+  
 }
--- src/app/shop/shop.page.ts
+++ src/app/shop/shop.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-shop',
@@ -9,7 +11,12 @@ import { ActivatedRoute } from '@angular/router';
 export class ShopPage implements OnInit {
 price:number = 0;
 name:string= "";
-  constructor(private router:ActivatedRoute) { }
+alert:string="";
+handlerMessage = "";
+roleMessage = "";
+
+
+  constructor(private router:ActivatedRoute, private alertController: AlertController, private toastController: ToastController) { }
 
   ngOnInit() {
     this.price = +this.router.snapshot.paramMap.get('price');
@@ -17,6 +24,43 @@ name:string= "";
     this.name = this.router.snapshot.paramMap.get('name');
     console.log(this.name)
 
+    //this.alert = this.router.snapshot.paramMap.get('alert');
+
+  }
+  
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Estas seguro/a que deseas comprar el producto?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          
+          handler: () => {
+            this.handlerMessage = 'Alert canceled';
+          },
+        },
+        {
+          text: 'Comprar',
+          
+          handler: () => {
+            this.buySuccess()
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    this.roleMessage = `Dismissed with role: ${role}`;
+  }
+
+  async buySuccess(){
+    const toast = await this.toastController.create({
+        message:'La compra ha sido exitosa',duration:4000,color:'success',mode:'ios',buttons:[{side:'start',icon:'cheapmark-circle'}]
+    });
+
+    toast.present()
   }
 
 }
